0b24177452a552cd4698eb595c22f35e3cc9cae4
Bhupesh1114 <78557475+Bhupesh1114@users.noreply.github.com>
2022-03-08 23:09:47 +0530

    Calculator-react-before-masai

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { mathematicalOperators ,numbers} from "./components/Calculator-buttons-value";
 import './App.css';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState("");
+
+  const input = (data) => {
+    setInputValue(oldItem => {
+      return oldItem + data;
+    })
+  }
+
+  const clear = () => {
+    setInputValue("");
+  }
+
+  const deleteFromEnd = () => {
+    setInputValue(inputValue => {
+      return inputValue.slice(0,inputValue.length -1)
+    })
+  }
+
+  const result = () => {
+    let evaluation = eval(inputValue);
+    setInputValue(evaluation.toString());
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Calculator</h1>
+      <div className="App-inner">
+      <div>
+      <input type="text" value={inputValue} placeholder="0"/>
+        </div>
+
+        <div className="upperButtons">
+        <div className="mathOperators">
+        {mathematicalOperators.map((item,index) => {
+        return <button onClick={() => input(item)} key={index}>{item}</button>
+        })}
+        </div>
+       <div className="delButton"><button onClick={deleteFromEnd}>Del</button></div>
+        </div>
+     
+        <div className="lowerButtons">
+          {numbers.map((item, index) => {
+            return <button onClick={() => input(item)} key={index}>{item}</button>
+          })}
+      </div>
+      
+        <div className="twoButtons">
+          <button onClick={clear}>Clear</button>
+          <button onClick={result}>=</button>
+        </div>
+      </div>
+     
     </div>
   );
 }
